d35e2140705282efb16f1ce997a935d220869d52
nakinor <nakinor@gmail.com>
2014-09-15 01:11:08 +0900

    remove utf8-string import in haskell

--- getsurei.hs
+++ getsurei.hs
@@ -1,7 +1,7 @@
 {-
    Author: nakinor
    Created: 2011-12-16
-   Revised: 2011-12-19
+   Revised: 2014-09-15
 
 月齢を計算して求めるプログラム
 
@@ -21,7 +21,7 @@
 import Data.Time.Format
 import Data.Time.LocalTime
 import System.Locale
-import System.IO.UTF8
+{- import System.IO.UTF8 -}
 
 main = do
   let a_array = [0,2,0,2,2,4,5,6,7,8,9,10]
@@ -35,6 +35,11 @@ main = do
   let c = (read a_day :: Int)
   let getsurei = show (mod (a + b + c) 30)
 
+{-
   System.IO.UTF8.putStrLn ("今日は" ++ a_year ++ "年" ++ a_month ++ "月"
                            ++ a_day ++ "日です。")
   System.IO.UTF8.putStrLn ("月齢は約" ++ getsurei ++ "日です。")
+-}
+  putStrLn ("今日は" ++ a_year ++ "年" ++ a_month ++ "月"
+                           ++ a_day ++ "日です。")
+  putStrLn ("月齢は約" ++ getsurei ++ "日です。")
