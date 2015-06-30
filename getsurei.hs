e87c46bdfab84c68744b7602343fad743e5505ab
nakinor <nakinor@gmail.com>
2015-06-30 23:01:51 +0900

    added comment

--- getsurei.hs
+++ getsurei.hs
@@ -1,23 +1,31 @@
 {-
    Author: nakinor
    Created: 2011-12-16
-   Revised: 2015-05-21
+   Revised: 2015-06-30
 
 月齢を計算して求めるプログラム
 
 実行はスクリプトとして
+
   ghci getsurei.hs
   main
+
 とするか
+
   runhaskell getsurei.hs
+
 もしくは実行ファイルを作って
+
   ghc getsurei.hs
   ./getsurei
+
+GHC を 7.8.3 にアップデートしたところ、utf8-string を import して使用する部分がエラーになってしまうので使わないようにした。標準に組込まれたのかな？
+
+GHC を 7.10.1 にアップデートしたところ、System.Locale を import するとエラーになってしまうので使わないようにした。何でかね？
 -}
 
 import Data.Time.Format
 import Data.Time.LocalTime
---import System.Locale
 
 main = do
   let a_array = [0,2,0,2,2,4,5,6,7,8,9,10]
@@ -32,5 +40,5 @@ main = do
   let getsurei = show (mod (a + b + c) 30)
 
   putStrLn ("今日は" ++ a_year ++ "年" ++ a_month ++ "月"
-                           ++ a_day ++ "日です。")
+            ++ a_day ++ "日です。")
   putStrLn ("月齢は約" ++ getsurei ++ "日です。")
